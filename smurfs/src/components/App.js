d26a91a0a1bef679f764e42fac066bd7578b2567
oredola <oredolaolamide@gmail.com>
2019-12-13 12:04:23 +0100

    Added event handlers for form input

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,13 +3,19 @@ import smurfsContext from "../contexts/smurfsContext";
 import axios from "axios";
 import "./App.css";
 import SmurfsList from "./SmurfsList";
+import Form from "./Form";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       apiData: [],
-      formValues: {}
+      formValues: {
+        name: "",
+        age: "",
+        height: "",
+        id: ""
+      }
     };
   }
 
@@ -19,7 +25,7 @@ class App extends Component {
       .then(res => {
         this.setState({
           ...this.state,
-          apiData: res.data,
+          apiData: res.data
         });
         console.log(this.state);
       })
@@ -28,16 +34,44 @@ class App extends Component {
       });
   }
 
+  handleFormChange = event => {
+    this.setState({
+      ...this.state,
+      formValues: {
+        ...this.state.formValues,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
+
+  handleSubmit = evt => {
+    axios
+      .post("http://localhost:3333/smurfs", this.state.formValues)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  };
+
   render() {
     return (
       <div className="App">
-          <h1>SMURFS! 2.0 W/O Redux</h1>
-          <div>Welcome to your state management version of Smurfs!</div>
-          {/* <div>Start inside of your `src/index.js` file!</div> */}
-          <div>I will have fun!</div>
+        <h1>SMURFS! 2.0 W/O Redux</h1>
+        <div>Welcome to your state management version of Smurfs!</div>
+        {/* <div>Start inside of your `src/index.js` file!</div> */}
+        <div>I will have fun!</div>
 
-        <smurfsContext.Provider value={this.state}>
+        <smurfsContext.Provider
+          value={{
+            ...this.state,
+            handleChange: this.handleFormChange,
+            handleSubmit: this.handleSubmit
+          }}
+        >
           <SmurfsList />
+          <Form />
         </smurfsContext.Provider>
       </div>
     );
--- smurfs/src/components/Form.js
+++ smurfs/src/components/Form.js
@@ -1,12 +1,23 @@
 import React, { useContext } from 'react';
 import smurfsContext from '../contexts/smurfsContext';
 
-function Form() {
+export default function Form() {
   // const data = useContext(smurfsContext);
 
   return (
-    <div>
-      pol
-    </div>
-  )
+    <form onSubmit={null}>
+      <label>
+        Name
+        <input name="name" onChange={null} />
+      </label>
+      <label>
+        Age
+        <input name="age" onChange={null} />
+      </label>
+      <label>
+        Height
+        <input name="height" onChange={null} />
+      </label>
+    </form>
+  );
 }
