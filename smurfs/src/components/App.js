5097633e8847da1174baa82c1ac401e1646b068f
oredola <oredolaolamide@gmail.com>
2019-12-13 11:16:35 +0100

    Context provider setup

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
-import SmurfsContext from '../components/App'
+import SmurfsContext from '../components/App';
+import axios from 'axios';
 import "./App.css";
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiData: [],
+      formValues: {}
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/O Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        {/* <div>Start inside of your `src/index.js` file!</div> */}
-        <div>I will have fun!</div>
-      </div>
+      <SmurfsContext.Provider value={this.state}>
+        <div className="App">
+          <h1>SMURFS! 2.0 W/O Redux</h1>
+          <div>Welcome to your state management version of Smurfs!</div>
+          {/* <div>Start inside of your `src/index.js` file!</div> */}
+          <div>I will have fun!</div>
+        </div>
+      </SmurfsContext.Provider>
     );
   }
 }
